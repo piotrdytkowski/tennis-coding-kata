8980297c0c32cf77ee05eac8db9eee9678fe465d
piotr.dytkowski <piotr.dytkowski@gmail.com>
2015-07-24 18:17:19 +0100

    Changing the game state

--- src/main/scala/tennis/model/GameState.scala
+++ src/main/scala/tennis/model/GameState.scala
@@ -3,3 +3,4 @@ package tennis.model
 sealed abstract class GameState
 
 case class InPlay() extends GameState
+case class GameFinished() extends GameState
--- src/main/scala/tennis/model/ScoreBoard.scala
+++ src/main/scala/tennis/model/ScoreBoard.scala
@@ -1,24 +1,28 @@
 package tennis.model
 
-case class ScoreBoard(score1: Score = Love(), score2: Score = Love()) {
+case class ScoreBoard(score1: Score, score2: Score, gameState: GameState = InPlay()) {
 
   def playerOneScored(): ScoreBoard = {
-    score1 match {
+    if (getState() == InPlay()) score1 match {
       case Love() => this.copy(score1 = Fifteen())
       case Fifteen() => this.copy(score1 = Thirty())
       case Thirty() if score2 != Fourty() => this.copy(score1 = Fourty())
       case Thirty() if score2 == Fourty() => ScoreBoard(Deuce(), Deuce())
-      case Fourty() => this.copy(score1 = GameWon())
+      case Fourty() => this.copy(score1 = GameWon(), gameState = GameFinished())
+    } else {
+      ???
     }
   }
 
   def playerTwoScored(): ScoreBoard = {
-    score2 match {
+    if (getState() == InPlay()) score2 match {
       case Love() => this.copy(score2 = Fifteen())
       case Fifteen() => this.copy(score2 = Thirty())
       case Thirty() if score1 != Fourty() => this.copy(score2 = Fourty())
       case Thirty() if score1 == Fourty() => ScoreBoard(Deuce(), Deuce())
-      case Fourty() => this.copy(score2 = GameWon())
+      case Fourty() => this.copy(score2 = GameWon(), gameState = GameFinished())
+    } else {
+      ???
     }
   }
 
@@ -27,6 +31,6 @@ case class ScoreBoard(score1: Score = Love(), score2: Score = Love()) {
   }
 
   def getState(): GameState = {
-    InPlay()
+    gameState
   }
 }
